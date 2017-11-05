80b940ca2f6be446df616968fcdc9ae98a5f82b4
George Antonious <george@antonious.ca>
2017-11-05 16:01:48 -0700

    Use builder based constraint definitions

--- Tripods/CameraViewController.swift
+++ Tripods/CameraViewController.swift
@@ -54,7 +54,6 @@ class CameraViewController: UIViewController {
         view.addSubview(button)
         view.addSubview(cameraControlsContainer)
         cameraControlsContainer.addSubview(lastPictureTakenImageView)
-        //view.addSubview(lastPictureTakenImageView)
         
         view.setNeedsUpdateConstraints()
     }
@@ -62,26 +61,81 @@ class CameraViewController: UIViewController {
     override func updateViewConstraints() {
         let margins = view.layoutMarginsGuide
         
-        textField.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 8.0).isActive = true
-        textField.topAnchor.constraint(equalTo: margins.topAnchor, constant: 80.0).isActive = true
-        textField.trailingAnchor.constraint(equalTo: button.leadingAnchor, constant: -8.0).isActive = true
+        _ = textField
+            .anchorLeading(to: margins.leadingAnchor, constant: 8.0)
+            .anchorTrailing(to: button.leadingAnchor, constant: -8.0)
+            .anchorTop(to: margins.topAnchor, constant: 80.0)
         
-        button.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.3).isActive = true
-        button.heightAnchor.constraint(equalTo: textField.heightAnchor, multiplier: 1.0).isActive = true
-        button.topAnchor.constraint(equalTo: textField.topAnchor).isActive = true
-        button.trailingAnchor.constraint(equalTo: margins.trailingAnchor, constant: -8.0).isActive = true
+        _ = button
+            .anchorWidth(to: margins.widthAnchor, multiplier: 0.3)
+            .anchorHeight(to: textField.heightAnchor, multiplier: 1.0)
+            .anchorTop(to: textField.topAnchor, constant: 0)
+            .anchorTrailing(to: margins.trailingAnchor, constant: -8.0)
         
-        cameraControlsContainer.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 1.0).isActive = true
-        cameraControlsContainer.heightAnchor.constraint(equalToConstant: 200.0).isActive = true
-        cameraControlsContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
-        cameraControlsContainer.bottomAnchor.constraint(equalTo: margins.bottomAnchor, constant: 0).isActive = true
-    
-        lastPictureTakenImageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor, constant: 8.0).isActive = true
-        lastPictureTakenImageView.centerYAnchor.constraint(equalTo: cameraControlsContainer.centerYAnchor, constant: 0).isActive = true
-        lastPictureTakenImageView.widthAnchor.constraint(equalToConstant: 30.0).isActive = true
-        lastPictureTakenImageView.heightAnchor.constraint(equalToConstant: 30.0).isActive = true
-
+        _ = cameraControlsContainer
+            .with(height: 100.0)
+            .anchorWidth(to: margins.widthAnchor, multiplier: 1.0)
+            .anchorLeading(to: margins.leadingAnchor, constant: 0)
+            .anchorBottom(to: margins.bottomAnchor, constant: 0)
+        
+        _ = lastPictureTakenImageView
+            .with(width: 50.0)
+            .with(height: 50.0)
+            .anchorLeading(to: cameraControlsContainer.leadingAnchor, constant: 8.0)
+            .alignCenterY(to: cameraControlsContainer.centerYAnchor, constant: 0)
         
         super.updateViewConstraints()
+    }
+}
+
+extension UIView {
+    func anchorLeading(to anchor: NSLayoutXAxisAnchor, constant c: CGFloat) -> UIView {
+        self.leadingAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
+    }
+    
+    func anchorTrailing(to anchor: NSLayoutXAxisAnchor, constant c: CGFloat) -> UIView {
+        self.trailingAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
+    }
+    
+    func anchorTop(to anchor: NSLayoutYAxisAnchor, constant c: CGFloat) -> UIView {
+        self.topAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
+    }
+    
+    func anchorBottom(to anchor: NSLayoutYAxisAnchor, constant c: CGFloat) -> UIView {
+        self.bottomAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
+    }
+    
+    func anchorWidth(to dimension: NSLayoutDimension, multiplier m: CGFloat) -> UIView {
+        self.widthAnchor.constraint(equalTo: dimension, multiplier: m).isActive = true
+        return self
+    }
+    
+    func anchorHeight(to dimension: NSLayoutDimension, multiplier m: CGFloat) -> UIView {
+        self.heightAnchor.constraint(equalTo: dimension, multiplier: m).isActive = true
+        return self
+    }
+    
+    func with(height: CGFloat) -> UIView {
+        self.heightAnchor.constraint(equalToConstant: height).isActive = true
+        return self
+    }
+    
+    func with(width: CGFloat) -> UIView {
+        self.widthAnchor.constraint(equalToConstant: width).isActive = true
+        return self
+    }
+    
+    func alignCenterX(to anchor: NSLayoutXAxisAnchor, constant c: CGFloat) -> UIView {
+        self.centerXAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
+    }
+    
+    func alignCenterY(to anchor: NSLayoutYAxisAnchor, constant c: CGFloat) -> UIView {
+        self.centerYAnchor.constraint(equalTo: anchor, constant: c).isActive = true
+        return self
     }
 }
