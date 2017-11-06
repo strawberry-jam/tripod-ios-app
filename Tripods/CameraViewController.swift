084f57b75382a378cad56a5d7d7060d786a560cf
George Antonious <george@antonious.ca>
2017-11-05 17:14:59 -0700

    Ensure photo permissions

--- Tripods/CameraViewController.swift
+++ Tripods/CameraViewController.swift
@@ -45,6 +45,27 @@ class CameraViewController: UIViewController {
         cameraControlsContainer.addSubview(cameraModelLabel)
         
         view.setNeedsUpdateConstraints()
+        
+        ensurePhotoPermissions {
+            fetchMostRecentPhoto { self.lastPictureTakenImageView.image = $0 }
+        }
+    }
+    
+    func ensurePhotoPermissions(_ doOnAuthorized: @escaping () -> Void) {
+        let authorizationStatus = PHPhotoLibrary.authorizationStatus()
+        
+        switch (authorizationStatus) {
+        case .authorized:
+            doOnAuthorized()
+        case .notDetermined:
+            PHPhotoLibrary.requestAuthorization({ status in
+                if status == .authorized {
+                    doOnAuthorized()
+                }
+            })
+        default:
+            return
+        }
     }
     
     override func updateViewConstraints() {
@@ -66,5 +87,26 @@ class CameraViewController: UIViewController {
             .anchorTrailing(to: cameraControlsContainer.trailingAnchor, constant: -10)
         
         super.updateViewConstraints()
+    }
+}
+
+
+import Photos
+
+func fetchMostRecentPhoto(callback: @escaping (UIImage?) -> Void) {
+    let fetchOptions = PHFetchOptions()
+    fetchOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
+    fetchOptions.fetchLimit = 1
+    
+    let fetchResult = PHAsset.fetchAssets(with: .image, options: fetchOptions)
+    
+    if let asset = fetchResult.firstObject {
+        PHImageManager.default().requestImage(for: asset,
+                                              targetSize: CGSize(width: asset.pixelWidth, height: asset.pixelHeight),
+                                              contentMode: .aspectFit,
+                                              options: nil,
+                                              resultHandler: { image, info in callback(image) })
+    } else {
+        callback(nil)
     }
 }
